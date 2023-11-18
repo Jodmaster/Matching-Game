9be3820cd4fe10f20f2659cacdc5eb948995b8df
Jodmaster <Rhysjohnson1@outlook.com>
2023-11-18 18:39:17 +0000

    started testing for level selection and scene changing

---
+++ Assets/Scripts/Start_game.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Start_game : MonoBehaviour
+{
+
+    public Button start_button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = start_button.GetComponent<Button>();
+        start_button.onClick.AddListener(loadLevelOne);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void loadLevelOne() {
+        Debug.Log("Level 1 loading");
+    }
+}
